f491ddfebbf2a0f4f18ad2e793d92d76280315e7
filhooliveira <ersijunior@gmail.com>
2020-02-29 14:28:05 -0300

    Importação inicial dos arquivos do projeto

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ajax-Php</title>
+	<link rel="stylesheet" type="text/css" href="ajaxphp/bootstrap-4.4.1/dist/css/bootstrap.css">
+	<script type="text/javascript" src="ajaxphp/bootstrap-4.4.1/dist/js/bootstrap.js" />
+	<script type="text/javascript" src="ajaxphp/bootstrap-4.4.1/dist/js/bootstrap.bundle.js" />
+</head>
+<body>
+	<div class="row">					
+	</div>
+	div.col-12	
+	
+
+</body>
+</html>
